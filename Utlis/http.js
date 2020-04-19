ae55af51ea8f6080ffbd9d6c83fcdd668bc92ff3
juhaodong <juhaodong@gmail.com>
2020-04-19 12:48:57 +0200

    准备重新封装请求类

--- Utlis/http.js
+++ Utlis/http.js
@@ -2,73 +2,112 @@ import axios from 'axios'
 import qs from 'qs'
 import Utils from './Utils.js'
 
-const instance = axios.create({timeout: 1000 * 30})
 
-export function initial(baseUrl) {
+
+const httpCode = { // 这里我简单列出一些常见的http状态码信息，可以自己去调整配置
+    400: '请求参数错误',
+    401: '权限不足, 请重新登录',
+    403: '服务器拒绝本次访问',
+    404: '请求资源未找到',
+    500: '内部服务器错误',
+    501: '服务器不支持该请求中使用的方法',
+    502: '网关错误',
+    504: '网关超时',
+}
+
+const RqConfig = {
+    Debug: process.env.NODE_ENV === 'production',
+    LoadingUtils: { showLoading: Utils.showLoading, hideLoading: Utils.hideLoading, showError: Utils.showError },
+}
+
+const JHRequestBase = {}
+
+const instance = axios.create({ timeout: 1000 * 30 })
+
+JHRequestBase.instance = instance
+JHRequestBase.initialed = false
+
+JHRequestBase.initial = function (baseUrl) {
     instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
-    instance.defaults.baseURL = baseUrl;
-    console.log(baseUrl)
+    instance.defaults.baseURL = baseUrl
+    JHRequestBase.initialed = true
+}
+
+export function initial (baseUrl) {
+    instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+    instance.defaults.baseURL = baseUrl
 }
 
 /**
  * 请求失败后的错误统一处理
- * @param {Number} status 请求失败的状态码
+ * @param {Object} error 请求失败
  */
-const errorHandle = (status) => {
-    // 状态码判断
-    switch (status) {
-        // 401: 未登录状态，跳转登录页
-        default:
+const errorHandle = (error) => {
+    const status = error.response.status
+    const tips = status in httpCode ? httpCode[status] : error.response.data.message
+    RqConfig.LoadingUtils.showError(tips)
+}
+
+const validateResponse = (res) => {
+    if (res.data.status) {
+        return res.data.status === 'good'
+    } else {
+        return true
     }
-};
+}
 
 instance.interceptors.request.use(
     req => {
         if (req.showLoading) {
-            Utils.showLoading()
+            RqConfig.LoadingUtils.showLoading()
         }
         if (req.method === 'get') {
-            console.log('Sending GET Request:', req.url, req.params)
+            if (RqConfig.Debug) {
+                console.info('------------------------------------------')
+                console.info('Sending GET Request:', req.url, req.params)
+            }
         }
 
         if (req.method === 'post' && req.headers.post['Content-Type']
             .includes('application/x-www-form-urlencoded')) {
-            console.log('Sending POST Request:', req.url, req.data);
+            if (RqConfig.Debug) {
+                console.log('Sending POST Request:', req.url, req.data)
+            }
             req.data = qs.stringify(req.data)
         }
         return req
     },
-    error => Promise.error(error));
+    error => Promise.error(error),
+)
+
 instance.interceptors.response.use(
     // 请求成功
     res => {
-        Utils.hideLoading();
-        console.log(res.data);
-        if (res.data.status) {
-            if (res.data.status === 'good') {
-                // console.log(res.data)
-                return Promise.resolve(res.data)
-            } else {
-                if (res.data.info) {
-                    Utils.showError(res.data.info)
-                }
-                return Promise.reject(res)
-            }
-        } else {
+        RqConfig.LoadingUtils.hideLoading()
+        if (RqConfig.Debug) {
             console.log(res.data)
+        }
+        if (validateResponse(res)) {
             return Promise.resolve(res.data)
+        } else {
+            if (res.data.info) {
+                RqConfig.LoadingUtils.showError(res.data.info)
+            } else {
+                RqConfig.LoadingUtils.showError()
+            }
+            return Promise.reject(res)
         }
     },
 // 请求失败
     error => {
-        const {response} = error;
+        RqConfig.LoadingUtils.hideLoading()
+        const { response } = error
         if (response) {
-            // 请求已发出，但是不在2xx的范围
-            errorHandle(response.status);
+            errorHandle(error)
             return Promise.reject(response)
         } else {
             return Promise.reject(error)
         }
     },
-);
-export {instance as http}
+)
+export { instance as http }

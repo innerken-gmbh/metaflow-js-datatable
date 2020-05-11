28bf56623504926619c0b9cb10dda186a7777e5a
Haodong Ju InnerKenMac <juhaodong@gmail.com>
2020-05-11 11:19:02 +0200

    添加DataEntityJS

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
-import {ego,IKUtils,hillo} from './index.js'
 import {IKDataEntity} from "./index";
-console.log(ego,IKUtils,hillo,IKDataEntity)
-hillo.post("https://www.baidu.com",{}).then(res=>{
+
+import {ego, hillo, IKUtils} from './index.js'
+
+
+console.log(ego, IKUtils, hillo, IKDataEntity)
+hillo.post("https://www.baidu.com", {}).then(res => {
     console.log(res)
 })

1383fefabd5129b9d2d4c271c6546d81470b18bc
juhaodong <juhaodong@gmail.com>
2020-05-16 19:40:32 +0200

    加入ModelFactory方法
    现在可以通过ModelFactory建立Model, 需要传入Entity和config参数,config可以包含load,add,edit,remove,getList,getOne这几个函数.

--- EntityFrameWork/DataEntity.js
+++ EntityFrameWork/DataEntity.js
@@ -1,4 +1,5 @@
 import Utils from '../Utlis/Utils.js'
+import { IKUtils } from '../index'
 
 export const Types = {
     Integer: Symbol('Type:Integer'),
@@ -9,7 +10,7 @@ export const Types = {
     Image: Symbol('Type:Image'),
     Time: Symbol('Type:Time'),
     Option: Symbol('Type:Option'),
-    getTypeDefault(type) {
+    getTypeDefault (type) {
         if (!type) {
             type = Types.String
         }
@@ -33,7 +34,7 @@ export const Types = {
             return undefined
         }
     },
-    parseValue(type, value) {
+    parseValue (type, value) {
         if (type === Types.Integer) {
             return parseInt(value)
         } else if (type === Types.Float) {
@@ -47,6 +48,62 @@ export const Types = {
 }
 Object.freeze(Types)
 
+async function generalLoad (url, data) {
+    return (await hillo.get(url, data))
+        .content.map(function (item) {
+            return IKDataEntity.parseDataForEntity(item, entity)
+        })
+}
+
+const generalGetOne = async function (asyncListFunc, conditionFunc) {
+    const _list = await asyncListFunc()
+    return _list.find(item => conditionFunc(item))
+}
+
+function ModelFactory (entity, config) {
+    let list = null
+
+    const load = async function (filter) {
+        return []
+    }
+    const add = function (item) {
+        return new Promise.reject('Add is not Definded')
+    }
+    const edit = function (item) {
+        return new Promise.reject('Add is not Definded')
+    }
+    const remove = function (id) {
+        return new Promise.reject('Add is not Definded')
+    }
+
+    const getList = async function (force = false, ...filter) {
+        if (!list || force) {
+            list = await load(filter)
+        }
+        return list
+    }
+
+    const getOne = async function (conditionFunc) {
+        return generalGetOne(getList, conditionFunc)
+    }
+
+    const DefaultConfig = {
+        getList,
+        add,
+        edit,
+        remove,
+        getOne,
+    }
+
+    config = IKUtils.extend(DefaultConfig, config)
+
+    return {
+        entity,
+        ...config,
+    }
+
+}
+
 const DefaultEntity = {
     value: '',
     displayName: '',
@@ -58,7 +115,7 @@ const DefaultEntity = {
         cols: 12,
         md: 6,
         sm: 12,
-        type: {name: 'text'},
+        type: { name: 'text' },
         // PossibleValue of types
         /*
         Text:{ name: 'text' }
@@ -86,11 +143,11 @@ const DefaultEntity = {
         requiredEdit: true,
         requiredNew: true,
     },
-    tableConfig: {overwrite: false},
+    tableConfig: { overwrite: false },
 }
 
 const TimeFormConfig = {
-    type: {name: 'time'},
+    type: { name: 'time' },
 }
 
 const OptionFormConfig = {
@@ -116,7 +173,7 @@ const BooleanFormConfig = {
     },
 }
 
-function generateEntity(_entity, key) {
+function generateEntity (_entity, key) {
     if (_entity.type === Types.Boolean) {
         if (_entity.formConfig) {
             if (_entity.formConfig.type) {
@@ -155,7 +212,7 @@ function generateEntity(_entity, key) {
     }
 }
 
-export function getFieldFromModel(model) {
+export function getFieldFromModel (model) {
     const field = []
     Object.keys(model.entity).forEach((key) => {
         field.push(generateEntity(model.entity[key], key))
@@ -167,7 +224,7 @@ export function getFieldFromModel(model) {
  * @param {*} item
  * @param {{}} structure
  */
-export function parseDataForEntity(item, structure) {
+export function parseDataForEntity (item, structure) {
     // console.log(structure)
     for (const key of Object.keys(structure)) {
         const instruction = structure[key]
@@ -186,7 +243,7 @@ export function parseDataForEntity(item, structure) {
  * @param { * } model
  * @return [header,formField,defaultItem]
  */
-export function parseField(model) {
+export function parseField (model) {
     const headers = []
     const formField = []
     const defaultItem = getFieldFromModel(model).reduce((map, item) => {
@@ -213,10 +270,9 @@ export function parseField(model) {
     return [headers, formField, defaultItem]
 }
 
-
 export default {
     parseDataForEntity,
     parseField,
     Types,
-    getFieldFromModel
+    getFieldFromModel,
 }

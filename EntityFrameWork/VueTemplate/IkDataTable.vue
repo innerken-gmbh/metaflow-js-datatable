d13db5ea3d0cb3b7166ebec7b792a5c2e1848bae
Haodong Ju <juhaodong@gmail.com>
2020-07-24 13:30:09 +0200

    移除无用的log以提升性能

--- EntityFrameWork/VueTemplate/IkDataTable.vue
+++ EntityFrameWork/VueTemplate/IkDataTable.vue
@@ -457,7 +457,6 @@ export default {
     },
 
     editItem (item) {
-      console.log(item)
       this.editedIndex = this.tableItem.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.$refs.gf.realDialog = true

3237a05e1754c701d9c63b3a56e4fd2afcd39372
Haodong Ju <juhaodong@gmail.com>
2020-10-26 16:36:16 +0100

    加入默认刷新功能，修改默认样式

--- EntityFrameWork/VueTemplate/FormField.vue
+++ EntityFrameWork/VueTemplate/FormField.vue
@@ -1,191 +1,192 @@
 <template>
-    <v-sheet
-            v-if="(currentState===-1&&inNew)||(currentState>-1&&inEdit)"
-    >
-        <template v-if="type.name==='text'">
-            <v-text-field
-                    :hide-details="noDetails"
-                    v-model="editedItem[value]"
-                    :disabled="shouldDisable"
-                    :rules="rules"
-                    :label="text"
-            />
+  <v-sheet
+      v-if="(currentState===-1&&inNew)||(currentState>-1&&inEdit)"
+  >
+    <template v-if="type.name==='text'">
+      <v-text-field
+          :hide-details="noDetails"
+          v-model="editedItem[value]"
+          :disabled="shouldDisable"
+          :rules="rules"
+          :label="text"
+      />
+    </template>
+    <template v-else-if="type.name==='select'">
+      <v-select
+          :hide-details="noDetails"
+          v-model="editedItem[value]"
+          :disabled="shouldDisable"
+          :items="selectItemList"
+          :item-text="type.itemText"
+          :item-value="type.itemValue"
+          :multiple="type.multiple"
+          :label="text"
+          :rules="rules"
+      />
+    </template>
+    <template v-else-if="type.name==='switch'">
+      <v-checkbox
+          hide-details
+          v-model="editedItem[value]"
+          :disabled="shouldDisable"
+          :label="text"
+          :rules="rules"
+      />
+    </template>
+    <template v-else-if="type.name==='image'">
+      <div>
+        <img-with-loading
+            v-if="editedItem[type.fileStorage]"
+            :height="'200px'"
+            :img-src="uploadUrl"
+        />
+        <template
+            v-else-if="currentState>-1"
+        >
+          <img-with-loading
+              :height="'200px'"
+              :img-src="root + editedItem[value]"
+          />
         </template>
-        <template v-else-if="type.name==='select'">
-            <v-select
-                    :hide-details="noDetails"
-                    v-model="editedItem[value]"
-                    :disabled="shouldDisable"
-                    :items="selectItemList"
-                    :item-text="type.itemText"
-                    :item-value="type.itemValue"
-                    :multiple="type.multiple"
-                    :label="text"
-                    :rules="rules"
-            />
+      </div>
+      <v-file-input
+          v-model="editedItem[type.fileStorage]"
+          :disabled="shouldDisable"
+          :label="text"
+          :rules="rules"
+          show-size
+          counter
+      />
+    </template>
+    <template v-else-if="type.name==='switch'">
+      <v-switch
+          v-model="editedItem[value]"
+          :disabled="shouldDisable"
+          :label="text"
+          :rules="rules"
+      />
+    </template>
+    <template v-else-if="type.name==='time'">
+      <v-dialog
+          ref="dialog"
+          v-model="timePickerShow"
+          :return-value.sync="editedItem[value]"
+          persistent
+          width="290px"
+      >
+        <template v-slot:activator="{ on }">
+          <v-text-field
+              v-model="editedItem[value]"
+              :label="text"
+              prepend-icon="mdi-clock-outline"
+              readonly
+              v-on="on"
+          />
         </template>
-        <template v-else-if="type.name==='switch'">
-            <v-switch
-                    v-model="editedItem[value]"
-                    :disabled="shouldDisable"
-                    :label="text"
-                    :rules="rules"
-            />
+        <v-time-picker
+            v-if="timePickerShow"
+            v-model="editedItem[value]"
+            full-width
+        >
+          <v-spacer/>
+          <v-btn
+              text
+              color="primary"
+              @click="timePickerShow = false"
+          >
+            {{ $t('Cancel') }}
+          </v-btn>
+          <v-btn
+              text
+              color="primary"
+              @click="$refs.dialog.save(editedItem[value])"
+          >
+            {{ $t('OK') }}
+          </v-btn>
+        </v-time-picker>
+      </v-dialog>
+    </template>
+    <template v-else-if="type.name==='date'">
+      <v-dialog
+          ref="dialog"
+          v-model="datePickerShow"
+          :return-value.sync="editedItem[value]"
+          persistent
+          width="290px"
+      >
+        <template v-slot:activator="{ on }">
+          <v-text-field
+              v-model="editedItem[value]"
+              :label="text"
+              prepend-icon="mdi-clock-outline"
+              readonly
+              v-on="on"
+          />
         </template>
-        <template v-else-if="type.name==='image'">
-            <div>
-                <img-with-loading
-                        v-if="editedItem[type.fileStorage]"
-                        :height="'200px'"
-                        :img-src="uploadUrl"
+        <v-date-picker
+            v-if="datePickerShow"
+            v-model="editedItem[value]"
+            full-width
+            :locale="locale"
+        >
+          <v-spacer/>
+          <v-btn
+              text
+              color="primary"
+              @click="datePickerShow = false"
+          >
+            {{ $t('Cancel') }}
+          </v-btn>
+          <v-btn
+              text
+              color="primary"
+              @click="$refs.dialog.save(editedItem[value])"
+          >
+            {{ $t('OK') }}
+          </v-btn>
+        </v-date-picker>
+      </v-dialog>
+    </template>
+    <template v-else-if="type.name==='color'">
+      <v-text-field
+          v-model=editedItem[value]
+          hide-details
+          :label="text"
+      >
+        <template
+            v-slot:append
+        >
+          <v-menu
+              v-model="colorPickerShow"
+              top
+              nudge-bottom="105"
+              nudge-left="16"
+              :close-on-content-click="false"
+          >
+            <template v-slot:activator="{ on }">
+              <div
+                  :style="swatchStyle"
+                  v-on="on"
+              />
+            </template>
+            <v-card>
+              <v-card-text
+                  class="pa-0"
+              >
+                <v-color-picker
+                    v-model="editedItem[value]"
+                    flat
                 />
-                <template
-                        v-else-if="currentState>-1"
-                >
-                    <img-with-loading
-                            :height="'200px'"
-                            :img-src="root + editedItem[value]"
-                    />
-                </template>
-            </div>
-            <v-file-input
-                    v-model="editedItem[type.fileStorage]"
-                    :disabled="shouldDisable"
-                    :label="text"
-                    :rules="rules"
-                    show-size
-                    counter
-            />
+              </v-card-text>
+            </v-card>
+          </v-menu>
         </template>
-        <template v-else-if="type.name==='switch'">
-            <v-switch
-                    v-model="editedItem[value]"
-                    :disabled="shouldDisable"
-                    :label="text"
-                    :rules="rules"
-            />
-        </template>
-        <template v-else-if="type.name==='time'">
-            <v-dialog
-                    ref="dialog"
-                    v-model="timePickerShow"
-                    :return-value.sync="editedItem[value]"
-                    persistent
-                    width="290px"
-            >
-                <template v-slot:activator="{ on }">
-                    <v-text-field
-                            v-model="editedItem[value]"
-                            :label="text"
-                            prepend-icon="mdi-clock-outline"
-                            readonly
-                            v-on="on"
-                    />
-                </template>
-                <v-time-picker
-                        v-if="timePickerShow"
-                        v-model="editedItem[value]"
-                        full-width
-                >
-                    <v-spacer/>
-                    <v-btn
-                            text
-                            color="primary"
-                            @click="timePickerShow = false"
-                    >
-                        {{ $t('Cancel') }}
-                    </v-btn>
-                    <v-btn
-                            text
-                            color="primary"
-                            @click="$refs.dialog.save(editedItem[value])"
-                    >
-                        {{ $t('OK') }}
-                    </v-btn>
-                </v-time-picker>
-            </v-dialog>
-        </template>
-        <template v-else-if="type.name==='date'">
-            <v-dialog
-                    ref="dialog"
-                    v-model="datePickerShow"
-                    :return-value.sync="editedItem[value]"
-                    persistent
-                    width="290px"
-            >
-                <template v-slot:activator="{ on }">
-                    <v-text-field
-                            v-model="editedItem[value]"
-                            :label="text"
-                            prepend-icon="mdi-clock-outline"
-                            readonly
-                            v-on="on"
-                    />
-                </template>
-                <v-date-picker
-                        v-if="datePickerShow"
-                        v-model="editedItem[value]"
-                        full-width
-                        :locale="locale"
-                >
-                    <v-spacer/>
-                    <v-btn
-                            text
-                            color="primary"
-                            @click="datePickerShow = false"
-                    >
-                        {{ $t('Cancel') }}
-                    </v-btn>
-                    <v-btn
-                            text
-                            color="primary"
-                            @click="$refs.dialog.save(editedItem[value])"
-                    >
-                        {{ $t('OK') }}
-                    </v-btn>
-                </v-date-picker>
-            </v-dialog>
-        </template>
-        <template v-else-if="type.name==='color'">
-            <v-text-field
-                    v-model=editedItem[value]
-                    hide-details
-                    :label="text"
-            >
-                <template
-                        v-slot:append
-                >
-                    <v-menu
-                            v-model="colorPickerShow"
-                            top
-                            nudge-bottom="105"
-                            nudge-left="16"
-                            :close-on-content-click="false"
-                    >
-                        <template v-slot:activator="{ on }">
-                            <div
-                                    :style="swatchStyle"
-                                    v-on="on"
-                            />
-                        </template>
-                        <v-card>
-                            <v-card-text
-                                    class="pa-0"
-                            >
-                                <v-color-picker
-                                        v-model="editedItem[value]"
-                                        flat
-                                />
-                            </v-card-text>
-                        </v-card>
-                    </v-menu>
-                </template>
-            </v-text-field>
-        </template>
-        <template v-else>
-            <slot/>
-        </template>
-    </v-sheet>
+      </v-text-field>
+    </template>
+    <template v-else>
+      <slot/>
+    </template>
+  </v-sheet>
 </template>
 
 <script>
@@ -225,20 +226,20 @@ export default {
     }
   },
   computed: {
-      swatchStyle () {
-          const { colorPickerShow } = this
-          return {
-              backgroundColor: this.editedItem.color,
-              cursor: 'pointer',
-              height: '30px',
-              width: '30px',
-              borderStyle: 'solid',
-              borderColor: '#c1c1c1',
-              borderWidth: '1px',
-              borderRadius: colorPickerShow ? '50%' : '4px',
-              transition: 'border-radius 200ms ease-in-out',
-          }
-      },
+    swatchStyle () {
+      const { colorPickerShow } = this
+      return {
+        backgroundColor: this.editedItem.color,
+        cursor: 'pointer',
+        height: '30px',
+        width: '30px',
+        borderStyle: 'solid',
+        borderColor: '#c1c1c1',
+        borderWidth: '1px',
+        borderRadius: colorPickerShow ? '50%' : '4px',
+        transition: 'border-radius 200ms ease-in-out',
+      }
+    },
     selectItemsIsDynamic: function () {
       return typeof this.type.selectItems === 'function'
     },
@@ -272,7 +273,7 @@ export default {
     },
     shouldDisable: function () {
       return (this.disableNew && this.isNew) ||
-        (this.disableEdit && !this.isNew)
+          (this.disableEdit && !this.isNew)
     },
     rules: function () {
 

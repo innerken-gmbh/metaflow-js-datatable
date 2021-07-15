413470b576ffccd53d8cc011692e034e9b0c50fa
Haodong Ju <juhaodong@gmail.com>
2021-07-15 15:56:35 +0200

    更新依赖试一试

--- EntityFrameWork/VueTemplate/IkDataTable.vue
+++ EntityFrameWork/VueTemplate/IkDataTable.vue
@@ -2,229 +2,212 @@
   <v-card
       class="mt-0"
   >
-    <v-toolbar dark dense>
-      <v-toolbar-title>
+    <v-card dark color="primary" class="d-flex align-center ma-0 px-4 py-1">
+      <div>
         <v-icon>{{ icon }}</v-icon>
         {{ entityName }}
-      </v-toolbar-title>
-      <v-btn @click="reload" icon>
-        <v-icon>mdi-refresh</v-icon>
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-pagination
-          v-model="page"
-          :length="pageCount"
-          :total-visible="5"
-      ></v-pagination>
-      <v-select class="flex-grow-0" hide-details style="width: 64px" v-model="itemsPerPage" :items="[-1,5,15,30]"></v-select>
-      <v-spacer></v-spacer>
-      <v-text-field
-          class="mr-2"
-          v-model="search"
-          append-icon="mdi-magnify"
-          hide-details
-          clearable
-          :label="$t('Search')"
-          single-line
-          style="max-width: 250px;"
-      />
-      <v-btn
-          color="primary"
-
-          dark
-          @click="$refs.gf.realDialog=true"
-      >
-        {{ $t(addText) }}
-      </v-btn>
-    </v-toolbar>
-    <slot name="extra-heading"/>
-    <v-toolbar
-        dense
-    >
-      <slot :items="items" name="filterLeft">
-      </slot>
-      <template v-if="mergableFields.length>0">
-        <template v-for="(field,index) in mergableFields">
-          <form-field
-              class="mx-1"
-              :no-details="true"
-              :key="index+'filter'"
-              :field="field"
-              :edited-item="filterItem"
-          />
-        </template>
-
-        <v-btn :color="Object.keys(filterItem).length>0?'error':''" icon @click="filterItem={}">
-          <v-icon>mdi-close-box</v-icon>
+        <v-btn @click="reload" text small>
+          <v-icon>mdi-refresh</v-icon>
         </v-btn>
-      </template>
-      <v-spacer/>
-      <v-toolbar-items>
-        <slot :items="items" :tableItems="tableItem" name="filterRight"></slot>
-      </v-toolbar-items>
-    </v-toolbar>
-    <v-data-table
-        dense
-        v-model="selectedItems"
-        :show-expand="showExpand"
-        :single-expand="singleExpand"
-        :show-select="useSelect"
-        :fixed-header="true"
-        :headers="realHeaders"
-        :items="tableItem"
-        :loading="loading"
-        :search.sync="search"
-        :height="'calc(100vh - '+(selectedItems.length>0?'248px)':'196px)')"
-        :page.sync="page"
-        :items-per-page="itemsPerPage"
-        hide-default-footer
-        @page-count="pageCount = $event"
-        multi-sort
-    >
-      <template
-          v-for="slottedItem in slottedItems"
-          v-slot:[slottedItem.name]="{ item }"
-      >
-        <slot
-            :name="slottedItem.name"
-            :item="item"
+      </div>
+      <v-spacer></v-spacer>
+      <div style="width: 150px;height: 100%;" class="d-flex align-center">
+        <v-text-field
+            class="mt-0 pt-0"
+            v-model="search"
+            append-icon="mdi-magnify"
+            hide-details
+            clearable
+            :label="$t('Search')"
+            single-line
+            style="max-width: 250px;"
         />
-      </template>
-      <template
-          v-for="adItem in advancedItems"
-          v-slot:[adItem.name]="{ item }"
-      >
-        <template
-            v-if="
-            adItem.dataType===Types.Image"
-        >
-          <img-template
-              :key="adItem.name"
-              :model="adItem.value"
-              :item="item"
-              :root="adItem.type.root()"
-          />
-        </template>
-        <template
-            v-else-if="
-            adItem.dataType===Types.Group"
-        >
-          <v-chip
-              v-bind:key="'_'+adItem.value+c"
-              v-for="(c) in adItem.childKey.filter(adItem.displayChild)"
-          >
-            {{ item['_' + adItem.value + c] }}
-          </v-chip>
-        </template>
-        <template
-            v-else-if="
-            adItem.dataType===Types.Boolean"
-        >
-          <v-simple-checkbox
-              :value="!!item[adItem.value]"
+      </div>
+    </v-card>
+    <slot name="extra-heading"/>
+    <v-card class="ma-0">
+      <div class="d-flex align-center">
+        <div style="width: fit-content">
+          <v-tabs height="36px"  v-model="tab">
+            <v-tab>数据</v-tab>
+            <v-tab>筛选</v-tab>
+          </v-tabs>
+        </div>
+        <v-spacer></v-spacer>
+        <div class="pr-4">应用的筛选</div>
+      </div>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <v-data-table
               dense
-              v-ripple
-              @click="toggleProperty(item,adItem.value)"
-              :key="adItem.name"
-          />
-        </template>
-        <template
-            v-else-if="
-            adItem.dataType===Types.Color"
-        >
-          <v-chip
-              :key="adItem.name"
-              :color="item[adItem.value]"
-              label
-              :style="swatchStyle"
-          />
-        </template>
-        <template
-            v-else-if="
-            adItem.dataType===Types.Option"
-        >
-          <template v-for="(l,index) of item['opt'+adItem.value]">
-            <v-chip
-                :key="item+adItem.name+'c'+index"
-                class="mx-1"
-                :color="adItem.type.color?
-                            adItem.type.color.find(c=>{return parseInt(item[adItem.value])===c.id}).color:''"
-                label
-            >
-              {{ l }}
-            </v-chip>
-          </template>
-        </template>
-      </template>
-      <template v-slot:footer>
-        <slot :items="items" :selectItems="selectedItems" name="footer">
-          <v-toolbar v-if="selectedItems.length>0"
-                     dense
-                     color="white"
+              v-model="selectedItems"
+              :show-expand="showExpand"
+              :single-expand="singleExpand"
+              :show-select="useSelect"
+              :fixed-header="true"
+              :headers="realHeaders"
+              :items="tableItem"
+              :loading="loading"
+              :search.sync="search"
+              :height="'calc(100vh - '+(selectedItems.length>0?'248px)':'196px)')"
+              multi-sort
           >
-            <template >
-              <template v-for="field in mergableFields.map(f=>({...f,cols:3,md:3,sm:3}))">
+            <template
+                v-for="slottedItem in slottedItems"
+                v-slot:[slottedItem.name]="{ item }"
+            >
+              <slot
+                  :name="slottedItem.name"
+                  :item="item"
+              />
+            </template>
+            <template
+                v-for="adItem in advancedItems"
+                v-slot:[adItem.name]="{ item }"
+            >
+              <template
+                  v-if="
+            adItem.dataType===Types.Image"
+              >
+                <img-template
+                    :key="adItem.name"
+                    :model="adItem.value"
+                    :item="item"
+                    :root="adItem.type.root()"
+                />
+              </template>
+              <template
+                  v-else-if="
+            adItem.dataType===Types.Group"
+              >
+                <v-chip
+                    v-bind:key="'_'+adItem.value+c"
+                    v-for="(c) in adItem.childKey.filter(adItem.displayChild)"
+                >
+                  {{ item['_' + adItem.value + c] }}
+                </v-chip>
+              </template>
+              <template
+                  v-else-if="
+            adItem.dataType===Types.Boolean"
+              >
+                <v-simple-checkbox
+                    :value="!!item[adItem.value]"
+                    dense
+                    v-ripple
+                    @click="toggleProperty(item,adItem.value)"
+                    :key="adItem.name"
+                />
+              </template>
+              <template
+                  v-else-if="
+            adItem.dataType===Types.Color"
+              >
+                <v-chip
+                    :key="adItem.name"
+                    :color="item[adItem.value]"
+                    label
+                    :style="swatchStyle"
+                />
+              </template>
+              <template
+                  v-else-if="
+            adItem.dataType===Types.Option"
+              >
+                <template v-for="(l,index) of item['opt'+adItem.value]">
+                  <v-chip
+                      :key="item+adItem.name+'c'+index"
+                      class="mx-1"
+                      :color="adItem.type.color?
+                            adItem.type.color.find(c=>{return parseInt(item[adItem.value])===c.id}).color:''"
+                      label
+                  >
+                    {{ l }}
+                  </v-chip>
+                </template>
+              </template>
+            </template>
+
+            <template v-slot:footer.prepend>
+              <v-btn
+                  v-if="useDefaultAction"
+                  color="primary"
+                  dark
+                  @click="$refs.gf.realDialog=true"
+              >
+                {{ $t(addText) }}
+              </v-btn>
+            </template>
+            <template v-slot:no-data>
+              <slot name="no-data">
+                <v-btn
+                    color="primary"
+                    @click="reload"
+                >
+                  {{ $t('重新加载') }}
+                </v-btn>
+              </slot>
+            </template>
+            <template v-slot:item.action="{ item }">
+              <slot
+                  name="item.action"
+                  :item="item"
+              />
+              <template v-if="useDefaultAction">
+                <template v-if="useEditAction">
+                  <v-icon
+                      x-large
+                      class="mr-2"
+                      @click="editItem(item)"
+                  >
+                    mdi-pencil-box
+                  </v-icon>
+                </template>
+                <template v-if="useDeleteAction">
+                  <v-icon
+                      x-large
+                      @click="deleteItem(item)"
+                  >
+                    mdi-delete
+                  </v-icon>
+                </template>
+              </template>
+
+            </template>
+            <template v-slot:expanded-item="{ item }">
+              <td :colspan="headers.length">
+                <slot
+                    name="expanded-item"
+                    :item="item"
+                />
+              </td>
+            </template>
+          </v-data-table>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card>
+            <slot :items="items" name="filterLeft">
+            </slot>
+            <template v-if="mergableFields.length>0">
+              <template v-for="(field,index) in mergableFields">
                 <form-field
                     class="mx-1"
                     :no-details="true"
-                    :key="field.id"
+                    :key="index+'filter'"
                     :field="field"
-                    :edited-item="mergeItem"
+                    :edited-item="filterItem"
                 />
               </template>
-              <v-btn @click="updateAll(mergeItem,false)" color="green">{{ $t('更新选中') }}</v-btn>
-              <v-btn @click="updateAll(null,true)" color="red">{{ $t('删除选中') }}</v-btn>
+              <v-btn :color="Object.keys(filterItem).length>0?'error':''" icon @click="filterItem={}">
+                <v-icon>mdi-close-box</v-icon>
+              </v-btn>
             </template>
-            <v-spacer/>
-
-          </v-toolbar>
-        </slot>
-      </template>
-      <template v-slot:no-data>
-        <slot name="no-data">
-          <v-btn
-              color="primary"
-              @click="reload"
-          >
-            {{ $t('重新加载') }}
-          </v-btn>
-        </slot>
-      </template>
-      <template v-slot:item.action="{ item }">
-        <slot
-            name="item.action"
-            :item="item"
-        />
-        <template v-if="useDefaultAction">
-          <template v-if="useEditAction">
-            <v-icon
-                x-large
-                class="mr-2"
-                @click="editItem(item)"
-            >
-              mdi-pencil-box
-            </v-icon>
-          </template>
-          <template v-if="useDeleteAction">
-            <v-icon
-                x-large
-                @click="deleteItem(item)"
-            >
-              mdi-delete
-            </v-icon>
-          </template>
-        </template>
-
-      </template>
-      <template v-slot:expanded-item="{ item }">
-        <td :colspan="headers.length">
-          <slot
-              name="expanded-item"
-              :item="item"
-          />
-        </td>
-      </template>
-    </v-data-table>
+            <slot :items="items" :tableItems="tableItem" name="filterRight"></slot>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
     <general-form
         ref="gf"
         :title="entityName"
@@ -250,70 +233,71 @@ export default {
   components: {
     ImgTemplate,
     GeneralForm,
-    FormField
+    FormField,
   },
   props: {
     model: {
       type: Object,
       default: () => {
-      }
+      },
     },
     addText: {
       type: String,
-      default: 'Add'
+      default: 'Add',
     },
     entityName: {
       type: String,
-      default: ''
+      default: '',
     },
     filter: {
       type: [Object, Function],
       default: () => {
-      }
+      },
     },
     icon: {
       type: String,
-      default: ''
+      default: '',
     },
     showExpand: {
       type: Boolean,
-      default: false
+      default: false,
     },
     singleExpand: {
       type: Boolean,
-      default: false
+      default: false,
     },
     useAction: {
       type: Boolean,
-      default: true
+      default: true,
     },
     useSelect: {
       type: Boolean,
-      default: true
+      default: true,
     },
     useDefaultAction: {
       type: Boolean,
-      default: true
+      default: true,
     },
     useEditAction: {
       type: Boolean,
-      default: true
+      default: true,
     },
     useDeleteAction: {
       type: Boolean,
-      default: true
-    }
+      default: true,
+    },
   },
   watch: {
     filter: {
       handler () {
         this.reload()
-      }
-    }
+      },
+    },
   },
   data: function () {
     return {
       page: 1,
+      tab:null,
       pageCount: 0,
       itemsPerPage: 15,
       filterItem: {},
@@ -332,7 +316,7 @@ export default {
       selectedItems: [],
       realHeaders: [],
       advancedItems: [],
-      slottedItems: []
+      slottedItems: [],
     }
   },
   computed: {
@@ -344,33 +328,33 @@ export default {
         borderStyle: 'solid',
         borderColor: '#c1c1c1',
         borderWidth: '1px',
-        transition: 'border-radius 200ms ease-in-out'
+        transition: 'border-radius 200ms ease-in-out',
       }
     },
     mergableFields: function () {
       return this.formField
-        .filter(item => [IKDataEntity.Types.Boolean, IKDataEntity.Types.Option].includes(item.dataType))
-        .filter(item => item.merge)
-        .map(item => {
-          return {
-            ...item,
-            name: 'item.' + item.value
-          }
-        })
+          .filter(item => [IKDataEntity.Types.Boolean, IKDataEntity.Types.Option].includes(item.dataType))
+          .filter(item => item.merge)
+          .map(item => {
+            return {
+              ...item,
+              name: 'item.' + item.value,
+            }
+          })
     },
     tableItem: function () {
       if (this.filterItem) {
         return this.items.filter(i => {
           return Object.keys(this.filterItem).every(
-            t => {
-              const org = i[t]
-              const oth = this.filterItem[t]
-              return org === oth || (Array.isArray(org) && (org.includes(oth) || oth.every(ot => org.includes(ot))))
-            })
+              t => {
+                const org = i[t]
+                const oth = this.filterItem[t]
+                return org === oth || (Array.isArray(org) && (org.includes(oth) || oth.every(ot => org.includes(ot))))
+              })
         })
       }
       return this.items
-    }
+    },
 
   },
   created () {
@@ -378,7 +362,7 @@ export default {
     if (this.useAction) {
       this.headers.push({
         text: 'action',
-        value: 'action'
+        value: 'action',
       })
     }
     this.realHeaders = this.getRealHeaders()
@@ -393,15 +377,15 @@ export default {
   methods: {
     getAdvancedItems: function () {
       return this.headers
-        .filter(item => [IKDataEntity.Types.Image, IKDataEntity.Types.Boolean,
-          IKDataEntity.Types.Option, IKDataEntity.Types.Group, IKDataEntity.Types.Color
-        ].includes(item.dataType))
-        .map(item => {
-          return {
-            ...item,
-            name: 'item.' + item.value
-          }
-        })
+          .filter(item => [IKDataEntity.Types.Image, IKDataEntity.Types.Boolean,
+            IKDataEntity.Types.Option, IKDataEntity.Types.Group, IKDataEntity.Types.Color,
+          ].includes(item.dataType))
+          .map(item => {
+            return {
+              ...item,
+              name: 'item.' + item.value,
+            }
+          })
     },
     getRealHeaders: function () {
       return this.headers.map(item => {
@@ -411,13 +395,13 @@ export default {
     },
     getSlottedItems: function () {
       return this.headers
-        .filter(item => item.overwrite)
-        .map(item => {
-          return {
-            ...item,
-            name: 'item.' + item.value
-          }
-        })
+          .filter(item => item.overwrite)
+          .map(item => {
+            return {
+              ...item,
+              name: 'item.' + item.value,
+            }
+          })
     },
     dialogChange (save) {
       if (save) {
@@ -475,21 +459,21 @@ export default {
     deleteItem (item, promt = true) {
       if (promt) {
         IKUtils.showConfirm(
-          this.$i18n.t('Are you sure?'),
-          this.$i18n.t('you want to delete this item?'), () => {
-            IKUtils.safeCallFunction(this.model, this.model.remove, item.id)
-              .then(() => {
-                IKUtils.toast(this.$i18n.t('删除成功'))
-                this.reload()
-              })
-          }
+            this.$i18n.t('Are you sure?'),
+            this.$i18n.t('you want to delete this item?'), () => {
+              IKUtils.safeCallFunction(this.model, this.model.remove, item.id)
+                  .then(() => {
+                    IKUtils.toast(this.$i18n.t('删除成功'))
+                    this.reload()
+                  })
+            },
         )
       } else {
         IKUtils.safeCallFunction(this.model, this.model.remove, item.id)
-          .then(() => {
-            IKUtils.toast(this.$i18n.t('删除成功'))
-            this.reload()
-          })
+            .then(() => {
+              IKUtils.toast(this.$i18n.t('删除成功'))
+              this.reload()
+            })
       }
     },
 
@@ -507,14 +491,7 @@ export default {
 
       this.loading = false
       this.$emit('reloaded')
-    }
-  }
+    },
+  },
 }
 </script>
-
-<style>
-.v-data-table td {
-  padding: 4px !important;
-}
-
-</style>
